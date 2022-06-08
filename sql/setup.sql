675464bbfcbca6d2b765c3666e56a104c4808526
Anddy <alonzoa2015@gmail.com>
2022-06-08 10:57:19 -0700

    table created

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS cats CASCADE;
+
+CREATE TABLE cats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    year INT NOT NULL,
+    lives INT NOT NULL,
+    isSidekick BOOLEAN NOT NULL,
+)
